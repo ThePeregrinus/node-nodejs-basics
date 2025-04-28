25a14c948b9dd63e0e8e201c6f81d746bbaf37a0
Mark Zaharov <zakharov.mark22@gmail.com>
2025-04-29 00:48:41 +0300

    add: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const argsKeyValue = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    argsKeyValue.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+  }
+
+  console.log(argsKeyValue.join(", "));
 };
 
 parseArgs();
