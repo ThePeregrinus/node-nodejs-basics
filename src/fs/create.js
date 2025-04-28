ac759fd67cd3c3fb987a8bbb19fd2e23270da2a1
Mark Zaharov <zakharov.mark22@gmail.com>
2025-04-28 21:00:38 +0300

    add: fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const directory = path.join(__dirname, "files", "fresh.txt");
+
+  const content = "I am fresh and young";
+
+  try {
+    await fs.writeFile(directory, content, { flag: "ax" });
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 await create();
