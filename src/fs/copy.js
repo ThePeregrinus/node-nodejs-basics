8a6fff927c14196dab84e65d9ce0cd83f1cddecd
Mark Zaharov <zakharov.mark22@gmail.com>
2025-04-28 23:12:54 +0300

    add: copy.js copies only files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from "fs/promises";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const copy = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const oldDirectoryPath = path.join(__dirname, "files");
+  const newDirectoryPath = path.join(__dirname, "files_copy");
+
+  const reccursiveFunc = async (oldPath, newPath) => {
+    const files = await fs.readdir(oldPath, "utf8");
+    await fs.mkdir(newPath);
+
+    files.forEach((file) => {
+      fs.writeFile(path.join(newDirectoryPath, file), file);
+    });
+
+    console.log(files);
+  };
+
+  try {
+    reccursiveFunc(oldDirectoryPath, newDirectoryPath);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
