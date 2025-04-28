89207f16898c606ca7e221b11cb21e5d6f9516fa
Mark Zaharov <zakharov.mark22@gmail.com>
2025-04-28 21:40:53 +0300

    add rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from "fs/promises";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const rename = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const oldFile = path.join(__dirname, "files", "wrongFilename.txt");
+  const newFIle = path.join(__dirname, "files", "properFilename.md");
+
+  try {
+    try {
+      await fs.access(newFIle);
+      throw new Error("FS operation failed: Destination file already exists");
+    } catch (e) {
+      if (e.code === "ENOENT") {
+        await fs.rename(oldFile, newFIle);
+      } else {
+        throw e;
+      }
+    }
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
