0939761cea252cb536b5051bbd8eeb938241e72e
Mark Zaharov <zakharov.mark22@gmail.com>
2025-04-29 01:48:56 +0300

    add streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import fs from "fs";
+import path from "path";
+import { pipeline } from "stream/promises";
+
 const read = async () => {
-    // Write your code here 
+  const dirname = import.meta.dirname;
+  const filePath = path.join(dirname, "files", "fileToRead.txt");
+
+  await pipeline(fs.createReadStream(filePath), process.stdout);
 };
 
 await read();
